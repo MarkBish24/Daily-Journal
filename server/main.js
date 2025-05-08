ba40d937c417d5d8a451eaa683a61d24ccda0d7f
MarkBish24 <markbish24@gmail.com>
2025-05-08 06:20:37 -0700

    simple set up

--- server/main.js
+++ server/main.js
@@ -3,13 +3,38 @@ const path = require("path");
 
 const fs = require("fs");
 
-function createWindow() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "Electron + React",
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+    icon: path.join(__dirname, "../renderer/assets/photos/notebook.png"),
   });
 
-  win.loadFile(path.join(__dirname, "../app/dist/index.html"));
+  const startUrl = process.env.VITE_DEV_SERVER_URL || "http://localhost:5173";
+
+  mainWindow.loadURL(startUrl);
 }
 
-app.whenReady().then(createWindow);
+function openTextEditorWindow(date) {
+  textEditorWindow = new BrowserWindow({
+    title: date,
+    width: 500,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+  textEditorWindow.loadURL(
+    process.env.VITE_DEV_SERVER_URL || "http://localhost:5173/editor"
+  );
+}
+
+app.whenReady().then(createMainWindow);
