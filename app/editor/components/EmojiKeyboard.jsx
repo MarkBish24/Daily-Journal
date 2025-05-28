a7ae9178f0fd2023354dda11f8141d1d07921774
MarkBish24 <markbish24@gmail.com>
2025-05-28 12:39:10 -0700

    updated and designed the emoji keyboard alittle bit

--- app/editor/components/EmojiKeyboard.jsx
+++ app/editor/components/EmojiKeyboard.jsx
@@ -1,25 +1,39 @@
 import React, { useState, useEffect } from "react";
 
+import { BsChevronBarUp } from "react-icons/bs";
+import { IoIosClose } from "react-icons/io";
+
 import emojis from "./emojis.js";
 import "./EmojiKeyboard.css";
 
 export default function EmojiKeyboard({ positions }) {
   return (
     <div
-      className="emoji-container"
+      className="keyboard-container"
       style={{
         position: "fixed",
         top: positions.y - 300,
         left: positions.x - 100,
+        zIndex: 1000,
       }}
     >
-      {emojis.map((emoji, index) => {
-        return (
+      <div className="keyboard-header-container">
+        <div className="drag-bar">
+          <BsChevronBarUp />
+        </div>
+
+        <button className="close-button">
+          <IoIosClose />
+        </button>
+      </div>
+
+      <div className="emoji-grid">
+        {emojis.map((emoji, index) => (
           <button className="emoji-button" key={index}>
             {emoji}
           </button>
-        );
-      })}
+        ))}
+      </div>
     </div>
   );
 }
